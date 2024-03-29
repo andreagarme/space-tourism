f5badb7b6514705c2cc499614516f36d761a6b91
andreagarme <andreagarme@gmail.com>
2024-03-29 19:35:09 +0000

    Creation of pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'destinations/:planet',
+    loadComponent: async () => {
+      const { DestinationsComponent } = await import(
+        './pages/destinations/destinations.component'
+      );
+      return DestinationsComponent;
+    },
+  },
+];
